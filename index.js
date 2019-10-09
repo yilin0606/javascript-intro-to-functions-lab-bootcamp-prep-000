237ee35b0f5aa943422937a6f482cd26d6c7c5f4
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-09 23:34:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string){
   return "I can\'t hear you!"}
   else if (string.toUpperCase()===string){
   return "YES INDEED!"}
-  else if(string==="I love you, Grandma"){
+  else if(string==="I love you, Grandma."){
     return "I love you, too."}
 }
